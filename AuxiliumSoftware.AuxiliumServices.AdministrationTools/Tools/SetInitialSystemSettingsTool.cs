55b45c353ef4755f2099e337fc68d8f4cb51ee37
Cerys <cerysamberlewis@gmail.com>
2026-06-05 13:56:21 +0100

    fixed the serialisation bug

--- AuxiliumSoftware.AuxiliumServices.AdministrationTools/Tools/SetInitialSystemSettingsTool.cs
+++ AuxiliumSoftware.AuxiliumServices.AdministrationTools/Tools/SetInitialSystemSettingsTool.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Spectre.Console;
 using Spectre.Console.Cli;
 using System.Security.Cryptography;
+using System.Text.Json;
 
 namespace AuxiliumSoftware.AuxiliumServices.AdministrationTools.Tools;
 
@@ -31,39 +32,17 @@ public sealed class SetInitialSystemSettingsTool(
             CreatedAt = DateTime.UtcNow,
             CreatedBy = null,
             ConfigKey = AuxiliumServices.Common.EntityFramework.Enumerators.SystemSettingKeyEnum.Instance_Branding_Name,
-            ConfigValue = AnsiConsole.Ask<string>("[green]instance.branding.name:[/]"),
+            ConfigValue = JsonSerializer.Serialize(AnsiConsole.Ask<string>("[green]instance.branding.name:[/]")),
             ValueType = AuxiliumServices.Common.EntityFramework.Enumerators.SystemSettingValueTypeEnum.Json,
             ReasonForModification = "Set through the Admin Tools CLI by a server administrator.",
         };
-        /*
-        SystemSettingEntityModel instance_branding_logoRelativePath = new()
-        {
-            Id = UUIDUtilities.GenerateV5(objectType: DatabaseObjectTypeEnum.System_SettingEntry),
-            CreatedAt = DateTime.UtcNow,
-            CreatedBy = null,
-            ConfigKey = AuxiliumServices.Common.EntityFramework.Enumerators.SystemSettingKeyEnum.Instance_Branding_LogoRelativePath,
-            ConfigValue = AnsiConsole.Ask<string>("[green]instance.branding.logoRelativePath:[/]"),
-            ValueType = AuxiliumServices.Common.EntityFramework.Enumerators.SystemSettingValueTypeEnum.Json,
-            ReasonForModification = "Set through the Admin Tools CLI by a server administrator.",
-        };
-        SystemSettingEntityModel instance_branding_logoContrastRelativePath = new()
-        {
-            Id = UUIDUtilities.GenerateV5(objectType: DatabaseObjectTypeEnum.System_SettingEntry),
-            CreatedAt = DateTime.UtcNow,
-            CreatedBy = null,
-            ConfigKey = AuxiliumServices.Common.EntityFramework.Enumerators.SystemSettingKeyEnum.Instance_Branding_LogoContrastRelativePath,
-            ConfigValue = AnsiConsole.Ask<string>("[green]instance.branding.logoContrastRelativePath:[/]"),
-            ValueType = AuxiliumServices.Common.EntityFramework.Enumerators.SystemSettingValueTypeEnum.Json,
-            ReasonForModification = "Set through the Admin Tools CLI by a server administrator.",
-        };
-        */
         SystemSettingEntityModel instance_navigation_portalBaseUrl = new()
         {
             Id = UUIDUtilities.GenerateV5(objectType: DatabaseObjectTypeEnum.System_SettingEntry),
             CreatedAt = DateTime.UtcNow,
             CreatedBy = null,
             ConfigKey = AuxiliumServices.Common.EntityFramework.Enumerators.SystemSettingKeyEnum.Instance_Navigation_PortalBaseUrl,
-            ConfigValue = AnsiConsole.Ask<string>("[green]instance.navigation.portalBaseUrl:[/]"),
+            ConfigValue = JsonSerializer.Serialize(AnsiConsole.Ask<string>("[green]instance.navigation.portalBaseUrl:[/]")),
             ValueType = AuxiliumServices.Common.EntityFramework.Enumerators.SystemSettingValueTypeEnum.Json,
             ReasonForModification = "Set through the Admin Tools CLI by a server administrator.",
         };
@@ -73,15 +52,13 @@ public sealed class SetInitialSystemSettingsTool(
             CreatedAt = DateTime.UtcNow,
             CreatedBy = null,
             ConfigKey = AuxiliumServices.Common.EntityFramework.Enumerators.SystemSettingKeyEnum.Instance_Fqdn,
-            ConfigValue = AnsiConsole.Ask<string>("[green]instance.fqdn:[/]"),
+            ConfigValue = JsonSerializer.Serialize(AnsiConsole.Ask<string>("[green]instance.fqdn:[/]")),
             ValueType = AuxiliumServices.Common.EntityFramework.Enumerators.SystemSettingValueTypeEnum.Json,
             ReasonForModification = "Set through the Admin Tools CLI by a server administrator.",
         };
 
 
         dbContext.System_Settings.Add(instance_branding_name);
-        //dbContext.System_Settings.Add(instance_branding_logoRelativePath);
-        //dbContext.System_Settings.Add(instance_branding_logoContrastRelativePath);
         dbContext.System_Settings.Add(instance_navigation_portalBaseUrl);
         dbContext.System_Settings.Add(instance_fqdn);
 
